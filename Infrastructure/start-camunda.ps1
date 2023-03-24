367361f64bebfefa95bfa14a829e46fce6fec9ad
bogdan petrushevski <bogdan@asseco-see.mk>
2023-03-24 10:52:33 +0100

    Initial commit

---
+++ Infrastructure/start-camunda.ps1
@@ -0,0 +1,12 @@
+docker run -d --name camunda -p 6060:8080 camunda/camunda-bpm-platform:run-latest
+
+
+# welcome page http://localhost:6060/camunda-welcome/index.html
+# u:demo p:demo
+
+
+# check services
+# http://localhost:3001/swagger
+# http://localhost:3002/swagger
+# http://localhost:3003/swagger
+# http://localhost:3004/swagger
